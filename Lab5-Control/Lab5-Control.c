e5149568cc3bb870dea787d2766fedf015b31870
Shane Copenhagen <copenhagenmhs@gmail.com>
2024-04-07 20:26:36 -0600

    slower period

--- Lab5-Control/Lab5-Control.c
+++ Lab5-Control/Lab5-Control.c
@@ -117,7 +117,7 @@ void Initialize_Modules( float _time_not_used_ )
     skid_controller.controller_left = left_controller;
     skid_controller.controller_right = right_controller;
 
-    Initialize_Skid_Steer(&skid_controller, b, a, 1, 0.01, 1.0, 1.0, 0.066, 0.039, NULL, NULL, NULL, NULL);
+    Initialize_Skid_Steer(&skid_controller, b, a, 1, 0.1, 1.0, 1.0, 0.066, 0.039, NULL, NULL, NULL, NULL);
     Initialize_Task(&task_update_controllers_vel, Send_Encoders_velocity);
 }
 
